6a58244543329affbd3d652e9610c3e371f12223
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-12-06 08:52:36 +0530

    search page added

--- src/main/java/com/example/productApp_Backend/controller/productController.java
+++ src/main/java/com/example/productApp_Backend/controller/productController.java
@@ -1,6 +1,7 @@
 package com.example.productApp_Backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,8 @@ public class productController {
     public  String add(){
         return "this is add page";
     }
-
+    @PostMapping("/search")
+    public String search() {
+        return "search product page";
+    }
 }
